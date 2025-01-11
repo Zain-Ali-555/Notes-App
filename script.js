4d764c891181b9c5e292db70fd9e562df81b8ba9
Zain Ali <zainchamp5@gmail.com>
2025-01-11 12:13:14 +0500

    textarea fixes

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let Apps = document.querySelector(".notesApp");
-let newBtn = document.querySelector(".i");
+let newBtn_Span = document.querySelector(".newBtn_Span");
+let newBtn = document.querySelector(".newBtn_Span .newBtn");
 
-newBtn.addEventListener("click", () => {
+newBtn_Span.addEventListener("click", () => {
+  const divNote = document.createElement("div");
+  divNote.className = "txtArea_Div";
   const newNote = document.createElement("textarea");
   newNote.className = "txtArea";
   newNote.title = "Enter Anything here";
   newNote.placeholder = "Take Notes";
-  Apps.appendChild(newNote);
+  Apps.appendChild(divNote);
+  divNote.appendChild(newNote);
 });
 
-newBtn.addEventListener
+newBtn_Span.addEventListener("mouseenter", () => {
+  newBtn.style.color = "#e7e9bb";
+  newBtn.style.backgroundColor = "transparent";
+});
+newBtn_Span.addEventListener("mouseleave", () => {
+  newBtn.style.color = "#403b4a";
+  newBtn.style.backgroundColor = "transparent";
+});
