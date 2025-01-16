840f66aa787e349698bb09451ebe5aad191c532e
Zain Ali <zainchamp5@gmail.com>
2025-01-16 18:11:24 +0500

    delete note btn added successfully

--- script.js
+++ script.js
@@ -41,10 +41,10 @@ newBtn_Span.addEventListener("click", () => {
   paintIcon.className = "paintIcon iblack ri-paint-fill";
   IconsDiv.appendChild(paintIcon);
 
-  // more Icon
-  const moreIcon = document.createElement("i");
-  moreIcon.className = "moreIcon iblack ri-more-2-fill";
-  IconsDiv.appendChild(moreIcon);
+  // delete Icon
+  let deleteIcon = document.createElement("i");
+  deleteIcon.className = "deleteIcon iblack ri-delete-bin-5-fill";
+  IconsDiv.appendChild(deleteIcon);
 
   // colorPLatee Opener
 
@@ -122,6 +122,25 @@ newBtn_Span.addEventListener("click", () => {
     }
   });
 
+  deleteIcon.addEventListener("click", (event) => {
+    let deleteNotification = document.createElement("div");
+    deleteNotification.className = "delete_Confirm_Note";
+    deleteNotification.innerHTML = `<h5> Do you really want to <span style="color: red;"> delete </span>this note?</h5>
+        <div class="btns_Div">
+          <button class="cncle btn">Cancel</button>
+          <button class="del btn">Delete</button>
+        </div>`;
+
+    console.log(event.target);
+    divNote.appendChild(deleteNotification);
+    deleteNotification.querySelector(".del").addEventListener("click", () => {
+      divNote.remove();
+    });
+    deleteNotification.querySelector(".cncle").addEventListener("click", () => {
+      deleteNotification.remove();
+    });
+  });
+
   // Increase Size of Note when open it
 
   newNote.addEventListener("click", (event) => {
@@ -129,7 +148,7 @@ newBtn_Span.addEventListener("click", () => {
       Apps.className = "opened";
       divNote.className = "opened_Note";
       document.querySelectorAll(".txtArea_Div").forEach((element) => {
-        element.style.display = "none"; // Hides all elements with class 'txtArea_Div'
+        element.className = "hidden"; // Hides all elements with class 'txtArea_Div'
       });
     }
     event.stopPropagation();
@@ -143,19 +162,7 @@ newBtn_Span.addEventListener("click", () => {
     Apps.className = "notesApp";
     divNote.className = "txtArea_Div";
     document.querySelectorAll(".txtArea_Div").forEach((element) => {
-      element.style.display = "inline"; // Hides all elements with class 'txtArea_Div'
+      element.className = "txtArea_Div"; // Hides all elements with class 'txtArea_Div'
     });
   });
 });
-
-function saveNotesToLocalStorage() {
-  const notes = [];
-  document.querySelectorAll(".txtArea_Div").forEach((note) => {
-    const textarea = note.querySelector(".txtArea");
-    notes.push({
-      content: textarea.value,
-      backgroundColor: note.style.backgroundColor || "white", // Save background color
-    });
-  });
-  localStorage.setItem("notes", JSON.stringify(notes));
-}
